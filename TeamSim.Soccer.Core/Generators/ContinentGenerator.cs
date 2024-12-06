64be46d1611e215ddff628a3f7c351d51b06f48d
UgoMezzogori <ugo.mezzogori@gmail.com>
2024-12-06 23:38:12 +0100

    added unittest and first generator

---
+++ TeamSim.Soccer.Core/Generators/ContinentGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using TeamSim.Sports.Soccer.Models;
+
+namespace TeamSim.Soccer.Core.Generators
+{
+    public class ContinentGenerator
+    {
+        public List<Continent> GenerateContinent()
+        {
+            string[] Continents = {
+                "Europe", "South America", "North America", "Asia", "Africa"
+            };
+
+
+            var clubFaker = new Faker<Continent>()
+                    .RuleFor(c => c.Name, f => f.Company.CompanyName() + " FC");
+
+            return clubFaker.Generate(5);
+        }
+    }
+}
